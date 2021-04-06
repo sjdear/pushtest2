eea343004a5f61a7256e3cd0d06b9d2ca34b2932
sjdear <67171108+sjdear@users.noreply.github.com>
2021-04-05 17:55:28 -0700

    auto-update 2021-04-05-17:55:28

--- update.js
+++ update.js
@@ -1,10 +1,13 @@
 
-const exec = require('child_process').exec;
-exec('/Users/sydneydeardorff/Desktop/pushtest050421/quickiePush.sh', (e, stdout, stderr)=> {
-    if (e instanceof Error) {
-        console.error(e);
-        throw e;
-    }
-    console.log('stdout', stdout);
-    console.log('sterr', stderr);
-});
+setInterval(function() {
+    const exec = require('child_process').exec;
+    exec('/Users/sydneydeardorff/Desktop/pushtest050421/quickiePush.sh', (e, stdout, stderr)=> {
+        if (e instanceof Error) {
+            console.error(e);
+            throw e;
+        }
+        console.log('stdout', stdout);
+        console.log('sterr', stderr);
+    });
+
+}, 20 * 1000);
